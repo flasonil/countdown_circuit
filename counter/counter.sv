be5995c8c6f0313382626c4f98603f92f32a87fa
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-04-02 10:11:58 +0200

    Update counter.sv

--- counter/counter.sv
+++ counter/counter.sv
@@ -1,1 +1,21 @@
 // counter.sv
+
+module counter
+  (
+    input logic start,
+    input logic clk,
+    
+    output logic ready
+  );
+  
+  logic q, enable, data, next_value;
+  
+  always@(posedge clk)
+    begin
+      if(enable)
+        q<=next_value
+      else
+        q<=q
+    end
+  
+endmodule
