d5be8b09bf552f68fa06f2e04306ed7f504b1923
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-04-02 14:57:52 +0200

    Update counter.sv

--- counter/counter.sv
+++ counter/counter.sv
@@ -8,7 +8,23 @@ module counter
     output logic ready
   );
   
-  logic q, enable, data, next_value;
+  logic q, enable, next_value;
+  
+  always_comb
+    begin
+      
+      if(q==0) begin
+        next_value=5'd8;
+        ready=1;
+        enable=start;
+      end
+      else begin
+        next_value=q-1;
+        ready=0;
+        enable=1;
+      end
+      
+    end
   
   always@(posedge clk)
     begin
