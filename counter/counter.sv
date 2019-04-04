b7aee100a3f3b33c41336352c8f009012f354108
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-04-04 23:12:09 +0200

    Stable update

--- counter/counter.sv
+++ counter/counter.sv
@@ -1,42 +1,39 @@
-// counter.sv
-
 module counter
-  (
-    input logic start,
-    input logic clk,
-    input logic rst,
-    
-    output logic ready
-  );
-  
-  logic q, enable, next_value;
-  
-  always_comb
-    begin
-      
-      if(q==0) begin
-        next_value=5'd8;
-        ready=1;
-        enable=start;
-      end
-      else begin
-        next_value=q-1;
-        ready=0;
-        enable=1;
-      end
-      
-    end
-  
-  always@(posedge clk)
-    begin
-      if(rst)
-        next_value<=0;
-      else begin
-        if(enable)
-          q<=next_value;
-        else
-          q<=q;
-      end
-    end
-  
+(
+	input logic start,
+	input logic clk,
+	input logic rst,
+
+	output logic ready,
+	output logic [4:0] q
+);
+
+logic [4:0] next_value;
+logic enable;
+
+always@(posedge clk) begin
+	if(rst) begin
+		q<=5'd0;
+		next_value<=5'd8;
+		enable<=1'd1;
+	end
+	else begin
+		if(enable)
+			q<=next_value;
+		else
+			q<=q;
+	end
+
+	if(q==0) begin
+		next_value=5'd8;
+		ready=1'd1;
+		enable=start;
+	end
+	else begin
+		next_value=q-5'd1;
+		ready=1'd0;
+		enable=1'd1;
+	end
+end
+
 endmodule
