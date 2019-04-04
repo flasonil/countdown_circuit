d0fe16d0197deb4edc38da23c91b3fb3555308a0
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-04-04 23:13:05 +0200

    Update testbench.sv

--- counter/testbench.sv
+++ counter/testbench.sv
@@ -1,48 +1,87 @@
+
+//Improve coverage testbench
 `timescale 1ns/1ps
+
+
 
 module testbench ();
 
 
-   logic  start;
-   logic  clk;
-   
-   logic  ready;
 
-   
 
-   counter dut ( .start(start), .clk(clk), .ready(ready) );
 
-   
+   logic  start,clk,rst,ready;
+   logic [4:0] q;
+
+   counter DUT ( .clk(clk), .start(start), .ready(ready), .rst(rst), .q(q) );
+
    always
+
+
 
      begin 
 
+
+
         #5 clk = !clk;
 
+
+
      end
+
+
+
 
 
    initial
 
+
+
      begin
 
-        clk = 0;
-        start = 0;
 
-        @(negedge clk);
-        
-        start = 1;
-        
-        @(negedge clk);
-        @(negedge clk);
-        
-        start = 0;
-        
-        @(negedge clk);
-        @(negedge clk);
 
-        $stop;
+        clk = 1'd0;
+
+        start = 1'd0;
+
+	rst = 1'd1;
+
+        #7 rst = 1'd0;
+
+	@(negedge clk);
+	@(negedge clk);
+	start = 1'd1;
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+	@(negedge clk);
+
+	$stop;
+
 
      end
-
 endmodule
